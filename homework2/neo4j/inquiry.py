a91cc7c335416992303766ed57d9509521dc799c
aroundabout <1051486637@qq.com>
2020-11-16 19:54:02 +0800

    第二次作业完整提交

--- homework2/neo4j/inquiry.py
+++ homework2/neo4j/inquiry.py
@@ -1,6 +1,7 @@
 # result = tx.run("match (u:User)-[r:REVIEW]-(p:Product)-[b:Belong]-(m:Movie) "
 #                 "where m.Movie=~\".*$message.*\""
 #                 "return u,count(r) order by count(r) desc", message=message)
+# mname = 'Tavener: Fall And Resurrection'
 
 from py2neo import Graph
 
@@ -11,16 +12,18 @@ graph = Graph(
 )
 
 mname = input("请输入要查询的关键词：")
-# mname = 'Tavener: Fall And Resurrection'
 print("结果是：")
 
 data1 = graph.run(
     "match (u:User)-[r:REVIEW]-(p:Product)-[b:Belong]-(m:Movie) where m.Movie=~\".*" + mname + ".*\" return u,count(r) order by count(r) desc"
 )
 maxCount = 0
+data = data1.data()
 result = set()
-for item in data1.data():
+for item in data:
     if item['count(r)'] >= maxCount:
         maxCount = item['count(r)']
         result.add(item['u'])
         print(item['u'])
+    else:
+        break
